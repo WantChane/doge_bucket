7fac1f8224cb02f5a093fcee773123bfa5d632dc
WantChane <143467549+WantChane@users.noreply.github.com>
2025-08-29 11:07:21 +0800

    feat(utils): add Sync-PersistFile

--- scripts/utils.ps1
+++ scripts/utils.ps1
@@ -87,6 +87,40 @@ function Remove-PersistFile {
     }
 }
 
+function Sync-PersistFile {
+    param(
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Name
+    )
+
+    $targetPath = Join-Path -Path $dir -ChildPath $Name
+    $persistPath = Join-Path -Path $persist_dir -ChildPath $Name
+
+    $targetExists = Test-Path -LiteralPath $targetPath -PathType Leaf
+    $persistExists = Test-Path -LiteralPath $persistPath -PathType Leaf
+
+    switch ($true) {
+        (-not $targetExists -and -not $persistExists) {
+            Write-Warning "Both files missing. Exiting."
+        }
+
+        ($targetExists -and -not $persistExists) {
+            $null = New-Item -ItemType HardLink -Path $persistPath -Target $targetPath
+            Write-Host "Created hard link from $targetPath to $persistPath" -ForegroundColor Cyan
+        }
+
+        (-not $targetExists -and $persistExists) {
+            $null = New-Item -ItemType HardLink -Path $targetPath -Target $persistPath
+            Write-Host "Created hard link from $persistPath to $targetPath" -ForegroundColor Cyan
+        }
+
+        ($targetExists -and $persistExists) {
+            Write-Host "Both files exist, Exiting." -ForegroundColor Green
+        }
+    }
+}
+
 function Test-ScheduledTask {
     [CmdletBinding()]
     param(
